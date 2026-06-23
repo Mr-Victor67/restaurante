fcc352ab763c1bf2fafeaed59010c69c36be0a9a
Mr-Victor67 <victor.matos.santana@escola.pr.gov.br>
2026-06-23 12:34:39 -0300

    componente e cardapio

---
+++ src/componentes/index.js
@@ -0,0 +1,20 @@
+function restaurantedeluxo(){
+return(
+    <section>
+<h1>resturante de luxo</h1>
+<h2>menu</h2>
+<p> lagosta com limão,farofa maionese e pure - R$649</p>
+<p>tubarão tigre fatiado em tiras com faria temperada e caviar - R$1745</p>
+<p>javali na braa com farofa, chope arroz temperado, pure e caviar - R$449</p>
+<p>carne wagyu feita no japão, eleita a carne mais cara do mundo e caviar - R$1520</p>
+
+<h2>menu-bebida</h2>
+
+<p>vinho seco importado da espanha - garrafa R$558, um copo R$150</p>
+<p>vinho doce importado da frança - grrafa R$763, um copo R$213</p>
+<p>champanhe importado da ítalia - garrafa - R$349, um copo R$97</p>
+</section>
+);
+
+}
+export default restaurantedeluxo;
